8c5bd5a1dcb969ed263562a3ee172d45b6a1f3fb
Rakesh Pai <rakeshpai@gmail.com>
2021-07-09 09:07:05 +0530

    Added getReleases

---
+++ server/network/azure-types.ts
@@ -0,0 +1,205 @@
+export type ProjectState = 'all' | 'createPending' | 'deleted' | 'deleting' | 'new' | 'unchanged' | 'wellFormed';
+export type ProjectVisibility = 'private' | 'public';
+export type DefinitionTypeStatus = 'disabled' | 'enabled' | 'paused';
+export type DefinitionType = 'build' | 'xaml';
+export type BuildStatus = 'all' | 'cancelling' | 'completed' | 'inProgress' | 'none' | 'notStarted' | 'postponed';
+export type BuildResult = 'canceled' | 'failed' | 'none' | 'partiallySucceeded' | 'succeeded';
+export type QueuePriority = 'aboveNormal' | 'belowNormal' | 'high' | 'low' | 'normal';
+export type BuildReason = 'all' | 'batchedCI' | 'buildCompletion' | 'checkInShelveset'
+| 'individualCI' | 'manual' | 'none' | 'pullRequest' | 'schedule' | 'scheduleForced'
+| 'triggered' | 'userCreated' | 'validateShelveset';
+export type ReleaseStatus = 'abandoned' | 'active' | 'draft' | 'undefined';
+export type ReleaseReason = 'continuousIntegration' | 'manual' | 'none' | 'pullRequest' | 'schedule';
+export type EnvironmentStatus = 'canceled' | 'inProgress' | 'notStarted'
+| 'partiallySucceeded' | 'queued' | 'rejected' | 'scheduled' | 'succeeded' | 'undefined';
+export type DeploymentReason = 'automated' | 'manual' | 'none' | 'redeployTrigger' | 'scheduled';
+export type DeploymentStatus = 'all' | 'failed' | 'inProgress' | 'notDeployed' | 'partiallySucceeded' | 'succeeded' | 'undefined';
+export type DeploymentOperationStatus = 'All' | 'Approved' | 'Canceled' | 'Cancelling'
+| 'Deferred' | 'EvaluatingGates' | 'GateFailed' | 'ManualInterventionPending'
+| 'Pending' | 'PhaseCanceled' | 'PhaseFailed' | 'PhaseInProgress'
+| 'PhasePartiallySucceeded' | 'PhaseSucceeded' | 'Queued' | 'QueuedForAgent'
+| 'QueuedForPipeline' | 'Rejected' | 'Scheduled' | 'Undefined';
+
+export type TeamProjectReference = {
+  id: string,
+  name: string,
+  description: string,
+  url: string,
+  state: ProjectState,
+  revision: number,
+  visibility: ProjectVisibility,
+  lastUpdatedTime: Date
+};
+
+export type DefinitionReference = {
+  createdDate?: Date,
+  id: number,
+  name: string,
+  path: string,
+  project: TeamProjectReference,
+  queueStatus: DefinitionTypeStatus,
+  revision: number,
+  type: DefinitionType,
+  uri: string,
+  url: string
+};
+
+export type IdentityRef = {
+  displayName: string;
+  url: string;
+  id: string;
+  uniqueName: string;
+  imageUrl: string;
+  descriptor: string;
+};
+
+export type AgentPoolQueue = {
+  id: number,
+  name: string,
+  pool: {
+    id: number,
+    name: string
+  }
+};
+
+export type GitRepository = {
+  id: string,
+  name: string,
+  url: string,
+  defaultBranch: string,
+  size: number,
+  remoteUrl: string,
+  sshUrl: string,
+  webUrl: string,
+  project: TeamProjectReference
+};
+
+export type BuildRepository = {
+  id: string;
+  type: string;
+  name: string;
+  url: string;
+  clean: null;
+  checkoutSubmodules: boolean;
+};
+
+export type Build = {
+  plans: { planId: string }[],
+  id: number,
+  buildNumber: string,
+  status: BuildStatus,
+  result: BuildResult,
+  queueTime: Date,
+  startTime: Date,
+  finishTime: Date,
+  url: string,
+  definition: DefinitionReference,
+  buildNumberRevision: number,
+  project: TeamProjectReference,
+  uri: string,
+  sourceBranch: string,
+  sourceVersion: string,
+  queue: AgentPoolQueue,
+  priority: QueuePriority,
+  reason: BuildReason,
+  requestedFor: IdentityRef
+  requestedBy: IdentityRef,
+  lastChangeDate: Date,
+  lastChangedBy: IdentityRef,
+  parameters: string,
+  orchestrationPlan: { planId: string },
+  repository: BuildRepository,
+  keepForever: boolean,
+  retainedByRelease: boolean,
+  triggeredByBuild: Build | null
+};
+
+export type DeploymentAttempt = {
+  id: number,
+  deploymentId: number,
+  attempt: number,
+  reason: DeploymentReason,
+  status: DeploymentStatus,
+  operationStatus: DeploymentOperationStatus,
+  // releaseDeploymentPhases: ReleaseDeployPhase[]
+  requestedBy: IdentityRef,
+  requestedFor: IdentityRef,
+  queueOn: Date,
+  lastModifiedBy: IdentityRef,
+  lastModifiedOn: Date,
+  hasStarted: boolean
+};
+
+export type ReleaseEnvironment = {
+  id: number,
+  releaseId: number,
+  name: string,
+  status: EnvironmentStatus,
+  deploySteps: DeploymentAttempt[],
+  rank: number,
+  definitionEnvironmentId: number,
+  // incomplete
+};
+
+export type ArtifactSourceReference = {
+  id: string,
+  name: string | null
+}
+
+export type Artifact = {
+  sourceId: string,
+  type: string,
+  alias: string,
+  definitionReference: {
+    artifactSourceDefinitionUrl: ArtifactSourceReference,
+    buildUri: ArtifactSourceReference,
+    definition: ArtifactSourceReference,
+    pullRequestSourceBranchCommitId: ArtifactSourceReference,
+    pullRequestId: ArtifactSourceReference,
+    pullRequestIterationId: ArtifactSourceReference,
+    pullRequestMergeCommitId: ArtifactSourceReference,
+    project: ArtifactSourceReference,
+    pullRequestSourceBranch: ArtifactSourceReference,
+    pullRequestTargetBranch: ArtifactSourceReference,
+    repository: ArtifactSourceReference,
+    requestedFor: ArtifactSourceReference,
+    requestedForId: ArtifactSourceReference,
+    branch: ArtifactSourceReference,
+    [x: string]: ArtifactSourceReference
+  }
+};
+
+export type ProjectReference = {
+  id: string,
+  name: string
+};
+
+export type ReleaseDefinitionShallowReference = {
+  id: number,
+  name: string,
+  path: string,
+  projectReference: ProjectReference | null,
+  url: string
+}
+
+export type Release = {
+  id: number,
+  name: string,
+  status: ReleaseStatus,
+  createdOn: Date,
+  modifiedOn: Date,
+  modifiedBy: IdentityRef,
+  createdBy: IdentityRef,
+  environments: ReleaseEnvironment[],
+  artifacts: Artifact[],
+  releaseDefinition: ReleaseDefinitionShallowReference,
+  releaseDefinitionRevision: number,
+  description: string,
+  reason: ReleaseReason,
+  releaseNameFormat: string,
+  keepForever: boolean,
+  definitionSnapshotRevision: number,
+  logsContainerUrl: string,
+  url: string,
+  projectReference: { id: string, name: string }
+};
--- server/network/azure.ts
+++ server/network/azure.ts
@@ -1,5 +1,6 @@
-import { GitRepository } from 'azure-devops-node-api/interfaces/TfvcInterfaces';
 import { Config } from '../types';
+import { pastDate } from '../utils';
+import { Build, GitRepository, Release } from './azure-types';
 import createPaginatedGetter from './create-paginated-getter';
 import { FetchResponse } from './fetch-with-disk-cache';
 
@@ -8,27 +9,64 @@ const apiVersion = { 'api-version': '5.1' };
 const flattenToValues = <T>(xs: FetchResponse<ListOf<T>>[]) => xs.flatMap(x => x.data.value);
 type ListOf<T> = { value: T[], count: number };
 
+const hasAnotherPage = <T>({ headers }: FetchResponse<T>) => (
+  Boolean(headers['x-ms-continuationtoken'])
+);
+
+const continuationToken = <T>(res: FetchResponse<T> | undefined): Record<string, never> | { continuationToken: string } => {
+  if (!res) return {};
+
+  const continuationToken = res.headers['x-ms-continuationtoken'];
+  if (!continuationToken) return {};
+  return { continuationToken };
+};
+
 export default (config: Config) => {
   const authHeader = {
     Authorization: `Basic ${Buffer.from(`:${config.token}`).toString('base64')}`
   };
   const paginatedGet = createPaginatedGetter(config);
-  const hasAnotherPage = <T>({ headers }: FetchResponse<T>) => (
-    Boolean(headers['x-ms-continuationtoken'])
-  );
   const url = (collectionName: string, projectName: string, path: string) => (
     `${config.host}${collectionName}/${projectName}/_apis${path}`
+  );
+  const list = <T>(
+    { url, qsParams, cacheFile }:
+    { url:string, qsParams?: Record<string, string>, cacheFile: string }
+  ) => (
+    paginatedGet<ListOf<T>>({
+      url,
+      qsParams: prev => ({ ...apiVersion, ...continuationToken(prev), ...qsParams }),
+      hasAnotherPage,
+      headers: () => authHeader,
+      cacheFile: pageIndex => `${cacheFile}_${pageIndex}`
+    }).then(flattenToValues)
   );
 
   return {
     getRepositories: (collectionName: string, projectName: string) => (
-      paginatedGet<ListOf<GitRepository>>({
+      list<GitRepository>({
         url: url(collectionName, projectName, '/git/repositories'),
-        qsParams: () => apiVersion,
-        hasAnotherPage,
-        headers: () => authHeader,
-        cacheFile: pageIndex => `${collectionName}_${projectName}_repositories_${pageIndex}`
-      }).then(flattenToValues)
+        cacheFile: `${collectionName}_${projectName}_repositories`
+      })
+    ),
+
+    getBuilds: (collectionName: string, projectName: string) => (
+      list<Build>({
+        url: url(collectionName, projectName, '/build/builds'),
+        qsParams: { minTime: pastDate(config.lookAtPast).toISOString() },
+        cacheFile: `${collectionName}_${projectName}_builds`
+      })
+    ),
+
+    getReleases: async (collectionName: string, projectName: string) => (
+      list<Release>({
+        url: url(collectionName, projectName, '/release/releases'),
+        qsParams: {
+          minCreatedTime: pastDate(config.lookAtPast).toISOString(),
+          $expand: 'environments,artifacts'
+        },
+        cacheFile: `${collectionName}_${projectName}_releases`
+      })
     )
   };
 };
