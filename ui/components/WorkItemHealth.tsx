dc2554efad6fb5c36e1288f1c471394a73d13bf4
Rakesh Pai <rakeshpai@gmail.com>
2021-08-16 13:02:47 +0530

    Semantics fix

--- ui/components/WorkItemHealth.tsx
+++ ui/components/WorkItemHealth.tsx
@@ -19,45 +19,48 @@ const WorkItem: React.FC<WorkItemProps> = ({
   const workItem = workItemsById[workItemId];
 
   return (
-    <button
+    <li
       className="bg-white border-l-4 p-6 mb-4 transition-colors duration-500 ease-in-out
-      rounded-lg shadow relative w-full text-left workitem-body"
-      onClick={() => setIsExpanded(!isExpanded)}
+      rounded-lg shadow relative workitem-body"
     >
-      <h3 className="flex justify-between">
-        <div className="w-4/5">
-          <a
-            href={workItem.url}
-            className="text-blue-600 font-bold text-lg truncate max-width-full inline-block hover:underline"
-            target="_blank"
-            rel="noreferrer"
-            title={workItem.title}
-          >
-            {workItem.title}
-          </a>
+      <button
+        className="w-full text-left"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <h3 className="flex justify-between">
+          <div className="w-4/5">
+            <a
+              href={workItem.url}
+              className="text-blue-600 font-bold text-lg truncate max-width-full inline-block hover:underline"
+              target="_blank"
+              rel="noreferrer"
+              title={workItem.title}
+            >
+              {workItem.title}
+            </a>
+          </div>
+          {isExpanded ? (
+            <span className="flex text-gray-500">
+              <span>Show less</span>
+              <UpChevron />
+            </span>
+          ) : (
+            <span className="flex text-gray-500">
+              <span className="show-more">Show more</span>
+              <DownChevron />
+            </span>
+          )}
+        </h3>
+        <div className="text-base font-normal text-gray-800">
+          <span className="text-blue-gray text-sm my-2">
+            Bundle size
+            {' '}
+            <span className="font-semibold text-base">{workItemsIdTree[workItemId].length}</span>
+          </span>
         </div>
-        {isExpanded ? (
-          <span className="flex text-gray-500">
-            <span>Show less</span>
-            <UpChevron />
-          </span>
-        ) : (
-          <span className="flex text-gray-500">
-            <span className="show-more">Show more</span>
-            <DownChevron />
-          </span>
-        )}
-      </h3>
-      <div className="text-base font-normal text-gray-800">
-        <span className="text-blue-gray text-sm my-2">
-          Bundle size
-          {' '}
-          <span className="font-semibold text-base">{workItemsIdTree[workItemId].length}</span>
-        </span>
-      </div>
+      </button>
       {isExpanded ? (
-        // eslint-disable-next-line jsx-a11y/no-static-element-interactions,  jsx-a11y/click-events-have-key-events
-        <div className="mt-4" onClick={e => e.stopPropagation()}>
+        <div className="mt-4 cursor-default">
           <WorkItemsGanttChart
             workItemId={workItemId}
             workItemsById={workItemsById}
@@ -66,7 +69,7 @@ const WorkItem: React.FC<WorkItemProps> = ({
           />
         </div>
       ) : null}
-    </button>
+    </li>
   );
 };
 
