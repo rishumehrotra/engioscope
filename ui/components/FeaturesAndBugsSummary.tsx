e8419ad841811e7677fe12ed3343908f4d4c27e0
Rakesh Pai <rakeshpai@gmail.com>
2021-10-01 14:56:40 +0530

    Adds a date-wise split to bug leakage/closed modals

--- ui/components/FeaturesAndBugsSummary.tsx
+++ ui/components/FeaturesAndBugsSummary.tsx
@@ -3,7 +3,9 @@ import { sum } from 'rambda';
 import type { ReactNode } from 'react';
 import React, { useCallback, useMemo, useState } from 'react';
 import type { AnalysedWorkItems, UIWorkItem, UIWorkItemType } from '../../shared/types';
-import { createPalette, num, oneYear } from '../helpers/utils';
+import {
+  createPalette, num, oneYear, shortDate
+} from '../helpers/utils';
 import { getCLTTime, getLeadTime } from '../helpers/work-item-utils';
 import { modalHeading, useModal } from './common/Modal';
 import HorizontalBarGraph from './graphs/HorizontalBarGraph';
@@ -22,10 +24,16 @@ const barColor = createPalette([
   '#795548'
 ]);
 
+type ShowModalArgs = {
+  title: ReactNode;
+  workItemIds: number[];
+  type: 'bug-leakage' | 'bugs-closed';
+};
+
 const computeBugLeakage = (
   bugLeakage: AnalysedWorkItems['bugLeakage'],
   workItemType: (workItem: UIWorkItem) => UIWorkItemType,
-  showInModal: (x: { title: ReactNode; workItemIds: number[] }) => void
+  showInModal: (x: ShowModalArgs) => void
 ): ProjectStatProps[] => {
   if (!bugLeakage) return [];
   const aggregated = Object.values(bugLeakage).reduce<{ opened: number; closed: number}>((acc, item) => ({
@@ -46,6 +54,7 @@ const computeBugLeakage = (
           .map(([type, bugs]) => ({ label: type, value: bugs.opened.length, color: barColor(type) }))}
         onBarClick={({ label }) => {
           showInModal({
+            type: 'bug-leakage',
             title: modalHeading('Bug leakage', label),
             workItemIds: bugLeakage[label].opened
           });
@@ -62,6 +71,7 @@ const computeBugLeakage = (
           .map(([type, bugs]) => ({ label: type, value: bugs.closed.length, color: barColor(type) }))}
         onBarClick={({ label }) => {
           showInModal({
+            type: 'bugs-closed',
             title: modalHeading('Bugs closed', label),
             workItemIds: bugLeakage[label].closed
           });
@@ -184,6 +194,26 @@ const getStats = (
     }, []);
 };
 
+const groupByDate = (
+  workItemIds: number[],
+  workItemById: (id: number) => UIWorkItem,
+  usingDate: (workItem: UIWorkItem) => string
+) => (
+  Object.entries(
+    workItemIds.reduce<Record<string, UIWorkItem[]>>((acc, id) => {
+      const workItem = workItemById(id);
+      const date = new Date(usingDate(workItem));
+      date.setHours(0, 0, 0, 0);
+      const dateString = date.toISOString();
+      acc[dateString] = acc[dateString] || [];
+      acc[dateString].push(workItem);
+      return acc;
+    }, {})
+  )
+    .map(([date, workItems]) => ({ date: new Date(date), workItems }))
+    .sort((a, b) => a.date.getTime() - b.date.getTime())
+);
+
 export type FeaturesAndBugsSummaryProps = {
   workItems: UIWorkItem[];
   bugLeakage: AnalysedWorkItems['bugLeakage'];
@@ -195,9 +225,9 @@ const FeaturesAndBugsSummary: React.FC<FeaturesAndBugsSummaryProps> = ({
   workItems, bugLeakage, workItemType, workItemById
 }) => {
   const [Modal, modalProps, open] = useModal();
-  const [modalContents, setModalContents] = useState<{ title: ReactNode; workItemIds: number[] } | null>(null);
+  const [modalContents, setModalContents] = useState<ShowModalArgs | null>(null);
 
-  const showInModal = useCallback((x: { title: ReactNode; workItemIds: number[] }) => {
+  const showInModal = useCallback((x: ShowModalArgs) => {
     setModalContents(x);
     open();
   }, [open]);
@@ -210,16 +240,39 @@ const FeaturesAndBugsSummary: React.FC<FeaturesAndBugsSummaryProps> = ({
   return (
     <div>
       <Modal {...modalProps} heading={modalContents?.title}>
-        <ol className="ml-8">
-          {modalContents?.workItemIds.map(id => (
-            <li key={id} className="py-2 list-decimal">
-              <WorkItemLinkForModal
-                workItem={workItemById(id)}
-                workItemType={workItemType(workItemById(id))}
-              />
-            </li>
-          ))}
-        </ol>
+        <ul className="">
+          {
+            groupByDate(
+              modalContents?.workItemIds || [],
+              workItemById,
+              modalContents?.type === 'bug-leakage'
+                ? (workItem: UIWorkItem) => workItem.created.on
+                // TODO: It's more accurate to use workitem
+                // TODO: closed date rather than updated date
+                : (workItem: UIWorkItem) => workItem.updated.on
+            )
+              .map(group => (
+                <li key={group.date.toISOString()} className="my-4">
+                  <h3 className="font-semibold text-lg pb-4">
+                    {shortDate(group.date)}
+                    <span className="inline-block py-0 px-4 ml-3 bg-gray-300 rounded-full">
+                      {group.workItems.length}
+                    </span>
+                  </h3>
+                  <ul className="ml-8 list-decimal">
+                    {group.workItems.map(workItem => (
+                      <li key={workItem.id} className="py-2">
+                        <WorkItemLinkForModal
+                          workItem={workItem}
+                          workItemType={workItemType(workItem)}
+                        />
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))
+          }
+        </ul>
       </Modal>
       <ProjectStats>
         {computedStats.map(stat => (
