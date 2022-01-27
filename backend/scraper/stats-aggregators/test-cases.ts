d724cf64025b25daa09dd14b8570be93ab7e83cc
Rakesh Pai <rakeshpai@gmail.com>
2022-01-27 12:14:39 +0530

    Added priority for test cases

--- backend/scraper/stats-aggregators/test-cases.ts
+++ backend/scraper/stats-aggregators/test-cases.ts
@@ -43,40 +43,40 @@ export default async (
 
   const [
     automatedTestCases,
-    notAutomatedTestCases
-    // automatedTestCasesP1,
-    // automatedTestCasesP2,
-    // automatedTestCasesP3,
-    // automatedTestCasesP4,
-    // automatedTestCasesP5,
-    // notAutomatedTestCasesP1,
-    // notAutomatedTestCasesP2,
-    // notAutomatedTestCasesP3,
-    // notAutomatedTestCasesP4,
-    // notAutomatedTestCasesP5
+    notAutomatedTestCases,
+    automatedTestCasesP1,
+    automatedTestCasesP2,
+    automatedTestCasesP3,
+    automatedTestCasesP4,
+    automatedTestCasesP5,
+    notAutomatedTestCasesP1,
+    notAutomatedTestCasesP2,
+    notAutomatedTestCasesP3,
+    notAutomatedTestCasesP4,
+    notAutomatedTestCasesP5
   ] = await Promise.all([
     getCount(queries.automatedTestCases(projectName)),
-    getCount(queries.notAutomatedTestCases(projectName))
-    // ...priorities.map(priority => getCount(queries.automatedTestCasesOfPriority(priority))),
-    // ...priorities.map(priority => getCount(queries.notAutomatedTestCasesOfPriority(priority)))
+    getCount(queries.notAutomatedTestCases(projectName)),
+    ...priorities.map(priority => getCount(queries.automatedTestCasesOfPriority(projectName, priority))),
+    ...priorities.map(priority => getCount(queries.notAutomatedTestCasesOfPriority(projectName, priority)))
   ]);
 
   return {
     automated: {
-      total: automatedTestCases
-      // p1: automatedTestCasesP1,
-      // p2: automatedTestCasesP2,
-      // p3: automatedTestCasesP3,
-      // p4: automatedTestCasesP4,
-      // p5: automatedTestCasesP5
+      total: automatedTestCases,
+      p1: automatedTestCasesP1,
+      p2: automatedTestCasesP2,
+      p3: automatedTestCasesP3,
+      p4: automatedTestCasesP4,
+      p5: automatedTestCasesP5
     },
     notAutomated: {
-      total: notAutomatedTestCases
-      // p1: notAutomatedTestCasesP1,
-      // p2: notAutomatedTestCasesP2,
-      // p3: notAutomatedTestCasesP3,
-      // p4: notAutomatedTestCasesP4,
-      // p5: notAutomatedTestCasesP5
+      total: notAutomatedTestCases,
+      p1: notAutomatedTestCasesP1,
+      p2: notAutomatedTestCasesP2,
+      p3: notAutomatedTestCasesP3,
+      p4: notAutomatedTestCasesP4,
+      p5: notAutomatedTestCasesP5
     }
   };
 };
