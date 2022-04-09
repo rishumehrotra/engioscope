5f36be35ea0b198ce0d88bc1653a47632325cb7f
Rakesh Pai <rakeshpai@gmail.com>
2022-04-09 13:40:32 +0530

    Removed strict mode due to issue with React Tooltip

--- ui/index.tsx
+++ ui/index.tsx
@@ -10,9 +10,9 @@ ReactModal.setAppElement('#root');
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
