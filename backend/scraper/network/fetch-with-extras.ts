9c3023d1a6a39559d06038b0ebf5c1e85d0afee1
Rakesh Pai <rakeshpai@gmail.com>
2022-05-20 13:54:54 +0530

    Added concurrency limit on outgoing requests

--- backend/scraper/network/fetch-with-extras.ts
+++ backend/scraper/network/fetch-with-extras.ts
@@ -2,24 +2,29 @@ import AbortController from 'abort-controller';
 import type { RequestInfo, RequestInit } from 'node-fetch';
 import fetch from 'node-fetch';
 import https from 'https';
+import pLimit from 'p-limit';
+
+const limit = pLimit(35);
 
 export default (url: RequestInfo, init?: RequestInit & { timeout?: number; verifySsl: boolean }) => {
   const { timeout = 60000, verifySsl, ...restInit } = init || {};
   const controller = new AbortController();
   const timeoutHandle = setTimeout(() => controller.abort(), timeout);
 
-  return fetch(url, {
-    ...restInit,
-    signal: controller.signal,
-    agent: verifySsl === false
-      ? new https.Agent({ rejectUnauthorized: false })
-      : undefined
-  })
-    .catch(err => {
-      if (err.name === 'AbortError') {
-        throw new Error(`Timeout: ${url} took longer than ${timeout / 1000}s to respond.`);
-      }
-      throw err;
+  return limit(() => (
+    fetch(url, {
+      ...restInit,
+      signal: controller.signal,
+      agent: verifySsl === false
+        ? new https.Agent({ rejectUnauthorized: false })
+        : undefined
     })
-    .finally(() => clearTimeout(timeoutHandle));
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          throw new Error(`Timeout: ${url} took longer than ${timeout / 1000}s to respond.`);
+        }
+        throw err;
+      })
+      .finally(() => clearTimeout(timeoutHandle))
+  ));
 };
