4eb67a141076ce313a539b6cf72c5c42d4f56745
Rakesh Pai <rakeshpai@gmail.com>
2022-06-08 10:58:14 +0530

    UI handling of overflowing roll-up stats

--- ui/components/ProjectStats.tsx
+++ ui/components/ProjectStats.tsx
@@ -2,15 +2,13 @@ import type { ReactNode } from 'react';
 import React from 'react';
 
 const ProjectStats: React.FC<{ note?: ReactNode; children: ReactNode }> = ({ children, note }) => (
-  <div className="overflow-x-auto">
-    <div className="justify-start grid grid-flow-row mb-3">
-      <div className="grid grid-flow-col relative gap-2">
-        {children}
-      </div>
-      {note ? (
-        <div className="text-xs text-gray-600 mt-1 ml-3">{note}</div>
-      ) : null}
+  <div className="justify-start grid grid-flow-row mb-8">
+    <div className="flex relative gap-2 flex-wrap">
+      {children}
     </div>
+    {note ? (
+      <div className="text-xs text-gray-600 mt-1 ml-3">{note}</div>
+    ) : null}
   </div>
 );
 
