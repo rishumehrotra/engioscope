52a192d2a898733648f805012ef1605b695c460c
Rakesh Pai <rakeshpai@gmail.com>
2022-07-25 21:28:27 +0530

    Trying

--- backend/dev/scrape.ts
+++ backend/dev/scrape.ts
@@ -1,7 +1,4 @@
 import scrape from '../scraper/get-all-the-things';
-// Following line ts-ignore'd so that it can work in the CI pipeline
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import config from '../../config.json';
 import type { Config } from '../scraper/parse-config';
 import parseConfig from '../scraper/parse-config';
--- backend/dev/start-server.ts
+++ backend/dev/start-server.ts
@@ -1,7 +1,4 @@
 import startServer from '../server/express';
-// Following line ts-ignore'd so that it can work in the CI pipeline
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import config from '../../config.json';
 import parseConfig from '../scraper/parse-config';
 
