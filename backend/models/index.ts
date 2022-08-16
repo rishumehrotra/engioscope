24552c17a6e9b44090b60188b4632dea3d24dbf0
Rakesh Pai <rakeshpai@gmail.com>
2022-08-16 16:19:27 +0530

    Mongoose cleanup

--- backend/models/index.ts
+++
@@ -1,6 +0,0 @@
-import { connect } from 'mongoose';
-import { listConnections } from './connections.js';
-
-await connect('mongodb://localhost:27017/test');
-const conns = await listConnections();
-console.log(conns);
