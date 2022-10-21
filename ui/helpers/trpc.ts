e4bad066a865c9c0e5781250f7701e825e8df769
Rakesh Pai <rakeshpai@gmail.com>
2022-10-21 17:06:06 +0530

    Moved to latest version of trpc

--- ui/helpers/trpc.ts
+++ ui/helpers/trpc.ts
@@ -1,4 +1,4 @@
-import { createTRPCReact } from '@trpc/react';
+import { createTRPCReact } from '@trpc/react-query';
 import type { AppRouter } from '../../backend/server/router';
 
 export const trpc = createTRPCReact<AppRouter>();
