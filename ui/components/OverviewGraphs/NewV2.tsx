92a7e1e88ce129827c7474f5f9ea155f6af5b8fc
Rakesh Pai <rakeshpai@gmail.com>
2022-11-02 18:01:31 +0530

    New graph wired up somewhat

--- ui/components/OverviewGraphs/NewV2.tsx
+++ ui/components/OverviewGraphs/NewV2.tsx
@@ -1,18 +1,68 @@
-import { sum } from 'rambda';
+import { prop, range, sum } from 'rambda';
 import React, { useMemo } from 'react';
 import { noGroup } from '../../../shared/work-item-utils.js';
 import { trpc } from '../../helpers/trpc.js';
-import { num } from '../../helpers/utils.js';
+import { num, shortDate } from '../../helpers/utils.js';
 import { useCollectionAndProject, useQueryPeriod } from '../../hooks/query-hooks.js';
 import useQueryPeriodDays from '../../hooks/use-query-period-days.js';
+import type { LineGraphProps } from '../graphs/LineGraph.jsx';
+import LineGraph from '../graphs/LineGraph.jsx';
 import Loading from '../Loading.jsx';
 import GraphCard from './helpers/GraphCard.jsx';
 import { lineColor, stringifyDateField } from './helpers/helpers.js';
 import type { LegendSidebarProps } from './helpers/LegendSidebar.jsx';
 import { LegendSidebar } from './helpers/LegendSidebar.jsx';
 
+type Point = {
+  date: Date;
+  value: number;
+};
+
+type Line = {
+  label: string;
+  workItemTypeName: string;
+  groupName: string;
+  points: Point[];
+};
+
 type NewGraphProps = {
   // openModal: (x: ModalArgs) => void;
+};
+
+const dataByDay = (
+  workItemSummary: Record<string, Record<string, Record<string, number>>>,
+  workItemTypes: { name: string[] }[],
+  queryPeriodDays: number
+) => {
+  const lines: Map<
+    { workItemType: string; groupName: string },
+    Map<string, number>
+  > = new Map(
+    Object.entries(workItemSummary)
+      .flatMap(([workItemTypeName, groups]) => Object.entries(groups).map(([groupName, dates]) => (
+        [{ workItemType: workItemTypeName, groupName }, new Map(Object.entries(dates))]
+      )))
+  );
+
+  return [...lines.entries()].map(([{ workItemType, groupName }, dates]): Line => ({
+    label: groupName === noGroup
+      ? workItemTypes.find(wit => wit.name[0] === workItemType)?.name[1] || workItemType
+      : `${workItemTypes.find(wit => wit.name[0] === workItemType)?.name[1]} - ${groupName}`,
+    workItemTypeName: workItemType,
+    groupName,
+    points: range(0, queryPeriodDays)
+      .reverse()
+      .map(daysOffset => {
+        const day = new Date();
+        day.setDate(day.getDate() - daysOffset);
+        const dayString = day.toISOString().split('T')[0];
+
+        return {
+          date: day,
+          value: dates.get(dayString) || 0
+        };
+      })
+  }));
 };
 
 const NewGraph: React.FC<NewGraphProps> = (/* { openModal } */) => {
@@ -33,28 +83,44 @@ const NewGraph: React.FC<NewGraphProps> = (/* { openModal } */) => {
       value: num(sum(Object.values(groups).flatMap(group => Object.values(group)))),
       unit: ''
     })),
-    items: Object.entries(workItemSummary.data || {}).flatMap(([workItemTypeName, groups]) => {
-      const matchingWit = workItemTypes.data?.find(wit => wit.name[0] === workItemTypeName);
+    items: Object.entries(workItemSummary.data || {})
+      .flatMap(([workItemTypeName, groups]) => {
+        const matchingWit = workItemTypes.data?.find(wit => wit.name[0] === workItemTypeName);
 
-      if (Object.keys(groups).length === 1 && Object.keys(groups)[0] === noGroup) {
-        return [{
+        if (Object.keys(groups).length === 1 && Object.keys(groups)[0] === noGroup) {
+          return [{
+            iconUrl: matchingWit?.icon || '',
+            label: matchingWit?.name[1] || workItemTypeName,
+            value: num(sum(Object.values(groups).flatMap(g => Object.values(g)))),
+            key: workItemTypeName,
+            color: lineColor({ groupName: noGroup, witId: workItemTypeName })
+          }];
+        }
+        return Object.entries(groups).flatMap(([groupName, byDate]) => ({
           iconUrl: matchingWit?.icon || '',
-          label: matchingWit?.name[1] || workItemTypeName,
-          value: num(sum(Object.values(groups).flatMap(g => Object.values(g)))),
-          key: workItemTypeName,
-          color: lineColor({ groupName: noGroup, witId: workItemTypeName })
-        }];
-      }
-      return Object.entries(groups).flatMap(([groupName, byDate]) => ({
-        iconUrl: matchingWit?.icon || '',
-        label: groupName,
-        value: num(sum(Object.values(byDate))),
-        key: `${workItemTypeName}-${groupName}`,
-        color: lineColor({ groupName, witId: workItemTypeName })
-      }));
-    }),
+          label: groupName,
+          value: num(sum(Object.values(byDate))),
+          key: `${workItemTypeName}-${groupName}`,
+          color: lineColor({ groupName, witId: workItemTypeName })
+        }));
+      }),
     onItemClick: key => console.log(key)
   }), [workItemSummary.data, workItemTypes.data]);
+
+  const lineGraphProps = useMemo<LineGraphProps<Line, Point>>(() => ({
+    className: 'max-w-full',
+    lines: dataByDay(workItemSummary.data || {}, workItemTypes.data || [], queryPeriodDays),
+    points: prop('points'),
+    pointToValue: point => point.value,
+    yAxisLabel: num,
+    lineLabel: line => line.label,
+    xAxisLabel: point => shortDate(point.date),
+    lineColor: line => lineColor({ groupName: line.groupName, witId: line.workItemTypeName }),
+    // crosshairBubble: showCrosshairBubble,
+    onClick: pointIndex => {
+      console.log(pointIndex);
+    }
+  }), [queryPeriodDays, workItemSummary.data, workItemTypes.data]);
 
   if (workItemSummary.isLoading) return <Loading />;
 
@@ -70,7 +136,7 @@ const NewGraph: React.FC<NewGraphProps> = (/* { openModal } */) => {
             {/* <SizeFilter workItems={preFilteredWorkItems} setFilter={setSizeFilter} />
             <PriorityFilter workItems={preFilteredWorkItems} setFilter={setPriorityFilter} /> */}
           </div>
-          {/* <LineGraph<WorkItemLine, WorkItemPoint> {...lineGraphProps} /> */}
+          <LineGraph<Line, Point> {...lineGraphProps} />
           <ul className="text-sm text-gray-600 pl-8 mt-4 list-disc bg-gray-50 p-2 border-gray-200 border-2 rounded-md">
             {(workItemTypes.data || [])
               .filter(wit => Object.keys(workItemSummary.data || {}).includes(wit.name[0]))
@@ -80,7 +146,7 @@ const NewGraph: React.FC<NewGraphProps> = (/* { openModal } */) => {
                   {`${
                     wit.name[0].toLowerCase().includes('bug')
                       ? 'created date'
-                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                       : stringifyDateField(wit.startDateFields!)
                   } within the last ${queryPeriodDays} days.`}
                 </li>
