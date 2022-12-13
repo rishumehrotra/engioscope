93b5eba15821ad39e72c299a7a9a5f64c8e16be0
Rakesh Pai <rakeshpai@gmail.com>
2022-12-13 20:24:44 +0530

    Upgraded vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [
+    react()
+  ],
   test: {
     include: ['**/__tests__/**.{ts,tsx}'],
     coverage: {
