92241a527d04d5dd0fc56176147b3523c36188ab
Rakesh Pai <rakeshpai@gmail.com>
2023-02-22 10:36:47 +0530

    Disabling commit cron

--- backend/crons/index.ts
+++ backend/crons/index.ts
@@ -5,7 +5,6 @@ import reposCron from './repos.js';
 import releaseDefinitionsCron from './release-definitions.js';
 import policyConfigurationCron from './policy-configuration.js';
 import branchesCron from './branches.js';
-import commitsCron from './commits.js';
 
 export default () =>
   [
@@ -16,5 +15,4 @@ export default () =>
     releaseDefinitionsCron,
     policyConfigurationCron,
     branchesCron,
-    commitsCron,
   ].forEach(fn => fn());
