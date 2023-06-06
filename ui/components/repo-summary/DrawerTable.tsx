fe6c9c24428f2f6e3abb4cf975ebf5daaf649354
Rakesh Pai <rakeshpai@gmail.com>
2023-06-06 23:50:32 +0530

    Sorting for the drawer table

--- ui/components/repo-summary/DrawerTable.tsx
+++ ui/components/repo-summary/DrawerTable.tsx
@@ -1,5 +1,7 @@
-import type { ReactNode } from 'react';
-import React, { Fragment, useState } from 'react';
+import type { MouseEvent, MouseEventHandler, ReactNode } from 'react';
+import React, { useMemo, Fragment, useState } from 'react';
+import { asc, desc } from 'sort-lib';
+import { ChevronRight, ArrowUp } from 'react-feather';
 import AnimateHeight from '../common/AnimateHeight.jsx';
 
 type DrawerTableProps<T> = {
@@ -13,6 +15,7 @@ type DrawerTableProps<T> = {
   ChildComponent?: React.FC<{ item: T }>;
   rowKey: (x: T) => string;
   isChild?: boolean;
+  defaultSortColumnIndex?: number;
 };
 
 const DrawerTable = <T,>({
@@ -21,14 +24,24 @@ const DrawerTable = <T,>({
   rowKey,
   ChildComponent,
   isChild = false,
+  defaultSortColumnIndex = 0,
 }: DrawerTableProps<T>) => {
   const [expandedRows, setExpandedRows] = useState<string[]>([]);
   const [collapsingRows, setCollapsingRows] = useState<string[]>([]);
+  const [sortColumnIndex, setSortColumnIndex] = useState<number>(defaultSortColumnIndex);
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>(
+    defaultSortColumnIndex === 0 ? 'asc' : 'desc'
+  );
+
+  const sortedData = useMemo(() => {
+    const { sorter } = columns[sortColumnIndex];
+    return data?.sort((sortDirection === 'asc' ? asc : desc)(sorter));
+  }, [columns, data, sortColumnIndex, sortDirection]);
 
   return (
     <table className={`w-full ${isChild ? 'bg-gray-50' : ''}`}>
       <thead>
-        <tr className="bg-gray-100 uppercase text-xs text-gray-600">
+        <tr className="bg-gray-100 text-xs text-gray-600">
           <th className="w-12"> </th>
           {columns.map((col, colIndex) => {
             return (
@@ -38,32 +51,66 @@ const DrawerTable = <T,>({
                   colIndex === 0 ? 'text-left' : 'text-right pr-4'
                 }`}
               >
-                {col.title}
+                <button
+                  className={isChild ? '' : 'uppercase'}
+                  onClick={() => {
+                    if (sortColumnIndex === colIndex) {
+                      return setSortDirection(d => (d === 'asc' ? 'desc' : 'asc'));
+                    }
+
+                    setSortColumnIndex(colIndex);
+                    setSortDirection(colIndex === 0 ? 'asc' : 'desc');
+                  }}
+                >
+                  {col.title}
+                  <span
+                    className={`inline-block -mb-[0.125em] ml-1 transition-transform duration-200 ${
+                      sortColumnIndex === colIndex
+                        ? `w-3 ${sortDirection === 'asc' ? 'rotate-180' : ''}`
+                        : 'mr-3'
+                    }`}
+                  >
+                    {sortColumnIndex === colIndex ? <ArrowUp size={12} /> : ''}
+                  </span>
+                </button>
               </th>
             );
           })}
         </tr>
       </thead>
       <tbody>
-        {data?.map(row => {
+        {sortedData?.map(row => {
           const rk = rowKey(row);
+
+          const isRowExpanded = expandedRows.includes(rk);
+
+          const toggleRow: MouseEventHandler = (evt: MouseEvent) => {
+            evt.stopPropagation();
+
+            if (isRowExpanded) {
+              setExpandedRows(rows => rows.filter(r => r !== rk));
+              setCollapsingRows(rows => [...rows, rk]);
+            } else {
+              setExpandedRows(rows => [...rows, rk]);
+            }
+          };
+
           return (
             <Fragment key={rk}>
-              <tr className="border-b border-gray-100">
+              <tr className="border-b border-gray-100 cursor-pointer" onClick={toggleRow}>
                 <td>
                   {ChildComponent ? (
                     <button
-                      className="bg-gray-300 block w-full h-full cursor-pointer"
-                      onClick={() => {
-                        if (expandedRows.includes(rk)) {
-                          setExpandedRows(rows => rows.filter(r => r !== rk));
-                          setCollapsingRows(rows => [...rows, rk]);
-                        } else {
-                          setExpandedRows(rows => [...rows, rk]);
-                        }
-                      }}
+                      className="block w-full h-full text-center"
+                      onClick={toggleRow}
+                      aria-label="Expand row"
                     >
-                      &gt;
+                      <ChevronRight
+                        size={18}
+                        className={`inline-block mb-[0.125rem] ml-4 transition-all duration-200 text-gray-400 ${
+                          isRowExpanded ? 'rotate-90 text-gray-500' : ''
+                        }`}
+                      />
                     </button>
                   ) : null}
                 </td>
@@ -74,8 +121,12 @@ const DrawerTable = <T,>({
                     <td
                       key={`${rk}-${col.key}`}
                       className={`${
-                        colIndex === 0 ? 'text-left' : 'text-right pr-4'
-                      } py-2`}
+                        colIndex === 0 ? 'text-left' : 'text-right pr-8'
+                      } py-3 ${
+                        colIndex === 0 && !expandedRows.includes(rk)
+                          ? ''
+                          : 'font-semibold'
+                      }`}
                     >
                       {colValue}
                     </td>
