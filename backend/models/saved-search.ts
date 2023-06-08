219141972e6fd8215a9e4b17c346a0fa2b0373e3
burdeamit@gmail.com <burdeamit@gmail.com>
2023-06-08 15:59:01 +0530

    Fixed Deleted Saved Search API

--- backend/models/saved-search.ts
+++ backend/models/saved-search.ts
@@ -5,19 +5,13 @@ import { SavedSearchModel } from './mongoose-models/SavedSearchModel.js';
 export const insertSavedSearch = (searchView: SavedSearch) =>
   SavedSearchModel.create(searchView);
 
-export const deleteSavedSearch = (collectionName: string, project: string, id: string) =>
-  SavedSearchModel.deleteOne({ collectionName, project, id }).then(x => x.deletedCount);
+export const deleteSavedSearch = (searchId: Types.ObjectId) =>
+  SavedSearchModel.deleteOne({ _id: searchId }).then(x => x.deletedCount);
 
 export const updateSavedSearch = (searchView: SavedSearch, searchId: Types.ObjectId) =>
-  SavedSearchModel.updateOne(
-    {
-      collectionName: searchView.collectionName,
-      project: searchView.project,
-      _id: searchId,
-    },
-    searchView,
-    { upsert: true }
-  ).then(x => x.upsertedId ?? null);
+  SavedSearchModel.updateOne({ _id: searchId }, searchView, { upsert: true }).then(
+    x => x.upsertedId ?? null
+  );
 
 export const getSavedSearches = (collectionName: string, project: string) =>
   SavedSearchModel.find({ collectionName, project }).lean().exec();
