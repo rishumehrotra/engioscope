57ab7a3ec48cccd4b505633ffea9ffd01d7e2e5c
Rakesh Pai <rakeshpai@gmail.com>
2023-07-26 15:42:22 +0530

    Tweaks to the config model

--- backend/models/mongoose-models/ConfigModel.ts
+++ backend/models/mongoose-models/ConfigModel.ts
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose';
 
 export type Config = {
   collectionName: string | null;
-  projectName: string | null;
+  project: string | null;
   filterBy?: {
     label: string;
     field: string[];
@@ -10,48 +10,53 @@ export type Config = {
   environments?: string[];
   templateRepoName?: string;
   workItemsConfig?: {
-    workItemType: string;
+    type: string;
     groupByField?: string;
     startState?: string[];
     endState?: string[];
     rootCause?: string[];
-    devCompletionState?: string;
+    devCompletionState?: string[];
+    ignoreStates: string[];
     workCenters?: {
       label: string;
-      startField?: string[];
-      endField?: string[];
+      startState?: string[];
+      endState?: string[];
     }[];
   }[];
 };
 
-const configSchema = new Schema<Config>({
-  collectionName: { type: String, default: null },
-  projectName: { type: String, default: null },
-  filterBy: [
-    {
-      label: { type: String, required: true },
-      field: [{ type: String, required: true }],
-    },
-  ],
-  environments: [String],
-  templateRepoName: String,
-  workItemsConfig: [
-    {
-      workItemType: { type: String, required: true },
-      groupByField: String,
-      startState: [String],
-      endState: [String],
-      rootCause: [String],
-      devCompletionState: String,
-      workCenters: [
-        {
-          label: String,
-          startField: [String],
-          endField: [String],
-        },
-      ],
-    },
-  ],
-});
+const configSchema = new Schema<Config>(
+  {
+    collectionName: { type: String, default: null },
+    project: { type: String, default: null },
+    filterBy: [
+      {
+        label: { type: String, required: true },
+        field: [{ type: String, required: true }],
+      },
+    ],
+    environments: [String],
+    templateRepoName: String,
+    workItemsConfig: [
+      {
+        type: { type: String, required: true },
+        groupByField: String,
+        startState: [String],
+        endState: [String],
+        rootCause: [String],
+        devCompletionState: [String],
+        ignoreStates: [String],
+        workCenters: [
+          {
+            label: String,
+            startState: [String],
+            endState: [String],
+          },
+        ],
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const ConfigModel = model<Config>('Config', configSchema);
