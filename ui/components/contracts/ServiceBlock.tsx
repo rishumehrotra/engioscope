a971bd7ac388023365a8c234153b5c4f3c2df8ad
Rakesh Pai <rakeshpai@gmail.com>
2023-10-10 13:36:03 +0530

    Trying the wrapper

--- ui/components/contracts/ServiceBlock.tsx
+++ ui/components/contracts/ServiceBlock.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useMemo } from 'react';
 import { twMerge } from 'tailwind-merge';
-import Xarrow from 'react-xarrows';
+import Xarrow, { Xwrapper } from 'react-xarrows';
 import { useQueryContext } from '../../hooks/query-hooks.js';
 import { trpc } from '../../helpers/trpc.js';
 import type { Service, ServiceAccessors } from './utils.jsx';
@@ -21,102 +21,104 @@ const ServiceBlock = ({ service, accessors }: ServiceBlockProps) => {
   const serviceRef = React.useRef<HTMLDivElement>(null);
 
   return (
-    <div className="overflow-x-scroll overflow-y-hidden w-auto relative">
-      <div className="inline-grid grid-flow-col items-center">
-        {/* Container for consumers */}
-        {consumers.length ? (
-          <div className="flex flex-row items-center">
-            {chunkArray(consumers, itemsPerColumn).map((column, columnIndex) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <ul key={columnIndex} className="mr-4 z-10">
-                {column.map(x => {
-                  const consumerId = `${service.serviceId}-consumer-${x.serviceId}`;
+    <Xwrapper>
+      <div className="overflow-x-scroll overflow-y-hidden w-auto relative">
+        <div className="inline-grid grid-flow-col items-center">
+          {/* Container for consumers */}
+          {consumers.length ? (
+            <div className="flex flex-row items-center">
+              {chunkArray(consumers, itemsPerColumn).map((column, columnIndex) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <ul key={columnIndex} className="mr-4 z-10">
+                  {column.map(x => {
+                    const consumerId = `${service.serviceId}-consumer-${x.serviceId}`;
 
-                  return (
-                    <Fragment key={x.serviceId}>
-                      <li
-                        data-tooltip-id="react-tooltip"
-                        data-tooltip-html={accessors.serviceTooltip(x)}
-                        className="border-r-[24px] border-theme-success text-right py-1 px-4 my-3 max-w-xs"
-                        id={consumerId}
-                      >
-                        <h3 className="font-medium">{x.name}</h3>
-                        <div className="text-theme-helptext text-sm">
-                          {x.endpoints.length}{' '}
-                          {minPluralise(x.endpoints.length, 'endpoint', 'endpoints')}
-                        </div>
-                      </li>
-                      <Xarrow
-                        start={consumerId}
-                        end={serviceRef}
-                        color="rgba(202, 138, 4, 0.2)"
-                        curveness={0.5}
-                        strokeWidth={20}
-                        headSize={3}
-                      />
-                    </Fragment>
-                  );
-                })}
-              </ul>
-            ))}
+                    return (
+                      <Fragment key={x.serviceId}>
+                        <li
+                          data-tooltip-id="react-tooltip"
+                          data-tooltip-html={accessors.serviceTooltip(x)}
+                          className="border-r-[24px] border-theme-success text-right py-1 px-4 my-3 max-w-xs"
+                          id={consumerId}
+                        >
+                          <h3 className="font-medium">{x.name}</h3>
+                          <div className="text-theme-helptext text-sm">
+                            {x.endpoints.length}{' '}
+                            {minPluralise(x.endpoints.length, 'endpoint', 'endpoints')}
+                          </div>
+                        </li>
+                        <Xarrow
+                          start={consumerId}
+                          end={serviceRef}
+                          color="rgba(202, 138, 4, 0.2)"
+                          curveness={0.5}
+                          strokeWidth={20}
+                          headSize={3}
+                        />
+                      </Fragment>
+                    );
+                  })}
+                </ul>
+              ))}
+            </div>
+          ) : null}
+          <div
+            ref={serviceRef}
+            data-tooltip-id="react-tooltip"
+            data-tooltip-html={accessors.serviceTooltip(service)}
+            className={twMerge(
+              consumers.length > 0 && 'ml-48',
+              providers.length > 0 && 'mr-32',
+              'border border-theme-input rounded-md px-12 py-3 min-w-fit text-center'
+            )}
+          >
+            <h3 className="font-medium">{service.name}</h3>
+            <div className="text-theme-helptext text-sm">
+              {service.endpoints.length}{' '}
+              {minPluralise(service.endpoints.length, 'endpoint', 'endpoints')}
+            </div>
           </div>
-        ) : null}
-        <div
-          ref={serviceRef}
-          data-tooltip-id="react-tooltip"
-          data-tooltip-html={accessors.serviceTooltip(service)}
-          className={twMerge(
-            consumers.length > 0 && 'ml-48',
-            providers.length > 0 && 'mr-32',
-            'border border-theme-input rounded-md px-12 py-3 min-w-fit text-center'
-          )}
-        >
-          <h3 className="font-medium">{service.name}</h3>
-          <div className="text-theme-helptext text-sm">
-            {service.endpoints.length}{' '}
-            {minPluralise(service.endpoints.length, 'endpoint', 'endpoints')}
-          </div>
+          {/* Container for providers */}
+          {providers.length ? (
+            <div className="flex flex-row items-center">
+              {chunkArray(providers, itemsPerColumn).map((column, columnIndex) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <ul key={columnIndex} className="ml-4">
+                  {column.map(x => {
+                    const providerId = `${service.serviceId}-provider-${x.serviceId}`;
+
+                    return (
+                      <Fragment key={x.serviceId}>
+                        <li
+                          id={providerId}
+                          data-tooltip-id="react-tooltip"
+                          data-tooltip-html={accessors.serviceTooltip(x)}
+                          className="border-l-[24px] border-theme-input-highlight text-left py-1 px-4 my-3 max-w-xs"
+                        >
+                          <h3 className="font-medium">{x.name}</h3>
+                          <div className="text-theme-helptext text-sm">
+                            {x.endpoints.length}{' '}
+                            {minPluralise(x.endpoints.length, 'endpoint', 'endpoints')}
+                          </div>
+                        </li>
+                        <Xarrow
+                          start={serviceRef}
+                          end={providerId}
+                          color="rgba(202, 138, 4, 0.2)"
+                          curveness={0.5}
+                          strokeWidth={20}
+                          headSize={3}
+                        />
+                      </Fragment>
+                    );
+                  })}
+                </ul>
+              ))}
+            </div>
+          ) : null}
         </div>
-        {/* Container for providers */}
-        {providers.length ? (
-          <div className="flex flex-row items-center">
-            {chunkArray(providers, itemsPerColumn).map((column, columnIndex) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <ul key={columnIndex} className="ml-4">
-                {column.map(x => {
-                  const providerId = `${service.serviceId}-provider-${x.serviceId}`;
-
-                  return (
-                    <Fragment key={x.serviceId}>
-                      <li
-                        id={providerId}
-                        data-tooltip-id="react-tooltip"
-                        data-tooltip-html={accessors.serviceTooltip(x)}
-                        className="border-l-[24px] border-theme-input-highlight text-left py-1 px-4 my-3 max-w-xs"
-                      >
-                        <h3 className="font-medium">{x.name}</h3>
-                        <div className="text-theme-helptext text-sm">
-                          {x.endpoints.length}{' '}
-                          {minPluralise(x.endpoints.length, 'endpoint', 'endpoints')}
-                        </div>
-                      </li>
-                      <Xarrow
-                        start={serviceRef}
-                        end={providerId}
-                        color="rgba(202, 138, 4, 0.2)"
-                        curveness={0.5}
-                        strokeWidth={20}
-                        headSize={3}
-                      />
-                    </Fragment>
-                  );
-                })}
-              </ul>
-            ))}
-          </div>
-        ) : null}
       </div>
-    </div>
+    </Xwrapper>
   );
 };
 
